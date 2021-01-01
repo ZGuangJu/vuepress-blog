3c1a8e1ef8fcb451d6be6bc1bf810cb948b4e6cf
ZGuangJu <guangjug@gmail.com>
2021-01-01 10:46:30 +0800

    提交了源码

---
+++ update.sh
@@ -0,0 +1,16 @@
+#.sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 开始提交源码
+
+cd D:/github/blog-vuepress
+git add .
+git commit -m '提交了源码'
+git push origin master
+# 提交源码到github结束
+
+git push -f git@gitee.com:zguangju/vuepress-blog.git master
+# 提交源码到gitee结束
+# 结束
