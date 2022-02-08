f1224cf9f864f5150a7b7a1e4ccc84b2ff07f866
gjzhangt <gjzhangt@isstech.com>
2022-02-08 11:02:37 +0800

    提交源码

--- update.sh
+++ update.sh
@@ -3,16 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 开始提交源码
+# 调整到项目目录中
 
 cd D:/Project/blog-vuepress
 
-# copy nul xxx.txt
+# 开始提交源码
+
 git add .
 git commit -m '提交源码'
 git push origin master
 # 提交源码到github结束
 
-# git push -f git@gitee.com:zguangju/vuepress-blog.git master
+git push -f git@gitee.com:zguangju/vuepress-blog.git master
 # 提交源码到gitee结束
 # 源码提交结束
