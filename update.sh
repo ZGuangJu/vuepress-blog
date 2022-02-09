3980b550fd8d9d6b7c46b14acbb354bf01647176
gjzhangt <gjzhangt@isstech.com>
2022-02-09 15:23:25 +0800

    提交源码

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -e
 
 # 调整到项目目录中
 
-cd D:/Project/blog-vuepress
+cd D:/Project/vuepress-blog
 
 # 开始提交源码
 
@@ -14,6 +14,6 @@ git commit -m '提交源码'
 git push origin master
 # 提交源码到github结束
 
-git push -f git@gitee.com:zguangju/vuepress-blog.git master
+git push -f git@gitee.com:zguangju/vuepress-blog.git dev
 # 提交源码到gitee结束
 # 源码提交结束
