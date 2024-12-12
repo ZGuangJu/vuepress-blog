d2e2922eacc65db777ca818811d7f2a54f524992
gjzhangt <gjzhangt@isstech.com>
2024-12-12 11:37:43 +0800

    修改1

--- test/index2.js
+++ test/index2.js
@@ -10,3 +10,9 @@ App.listen(3000, () => { console.log("已启动-localhost:3000"); })
 App.get('/', (req, res) => {
     res.json({ code: 1, msg: ok })
 })
+// 允许跨域的地址数组
+// App.use(cors({ origin: ["https://xxxx", "https://xxxxxx"] }))
+App.listen(3000, () => { console.log("已启动-localhost:3000"); })
+App.get('/', (req, res) => {
+    res.json({ code: 1, msg: ok })
+})
