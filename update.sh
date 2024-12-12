4856c1c2cadf76d5e12611fa42a31903af27d839
ZGuangJu <guangjug@gmail.com>
2024-12-12 13:51:41 +0800

    修改 updated20241212_xk

--- update.sh
+++ update.sh
@@ -1,22 +1,28 @@
 #.sh
-
+#!/bin/bash
+# 获取当前日期，格式为 YYYYMMDD
+current_date=$(date +%Y%m%d)
 # 确保脚本抛出遇到的错误
 set -e
-
-# 调整到项目目录中
-
+#!/bin/bash
+#echo "Hello World !"
+#
+## 调整到项目目录中
+#
 cd D:/Project/vuepress-blog
-
-# 开始提交源码
-
+#echo "Hello World !"
+#
+## 开始提交源码
+#
 git add .
-git commit -m '修改'
+#git commit -m '修改'
+git commit -m "修改 updated${current_date}_xk"
 
-# git push origin dev
 git push -f git@gitee.com:zguangju/vuepress-blog.git master
-# 提交源码到gitee结束
-
+## 提交源码到gitee结束
+#
 git push -f git@github.com:ZGuangJu/vuepress-blog.git master
-# 提交源码到github结束
-
-# 源码提交结束
+## 提交源码到github结束
+#
+## 源码提交结束
+sleep 10000
